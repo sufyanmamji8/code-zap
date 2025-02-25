18a63d551f624c7e6bd1ba5e207836a88f5f7f5d
DESKTOP-A2KQDFJ\scb <hamza.mamji@codovio.com>
2025-02-25 14:14:07 +0500

    .

--- src/Api/Constant.js
+++ src/Api/Constant.js
@@ -1,5 +1,5 @@
 // export const USER_API_ENDPOINT = 'https://codozap-e04e12b02929.herokuapp.com/api/v1/user';
-export const USER_API_ENDPOINT = 'https://codozap-e04e12b02929.herokuapp.com//api/v1/user';
+export const USER_API_ENDPOINT = 'https://codozap-e04e12b02929.herokuapp.com/api/v1/user';
 
 // export const COMPANY_API_ENDPOINT = 'https://codozap-e04e12b02929.herokuapp.com/api/v1/company';
 export const COMPANY_API_ENDPOINT = 'https://codozap-e04e12b02929.herokuapp.com/api/v1/company';
